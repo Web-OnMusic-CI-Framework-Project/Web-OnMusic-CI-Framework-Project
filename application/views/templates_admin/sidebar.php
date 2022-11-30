1791666251d8c9560278850de395cd5f574c3955
Anggun Lenteraningati <anggunlentera@gmail.com>
2022-11-30 14:49:36 +0700

    add folder templates admin di dalam folder views

---
+++ application/views/templates_admin/sidebar.php
@@ -0,0 +1,52 @@
+<body>
+    <div id="app">
+        <div class="main-wrapper main-wrapper-1">
+            <div class="navbar-bg color-background-navbar"></div>
+            <nav class="navbar navbar-expand-lg main-navbar">
+                <form class="form-inline mr-auto">
+                    <div class="search-element">
+                        <input class="form-control" type="search" placeholder="Search" aria-label="Search" data-width="250">
+                        <button class="btn" type="submit"><i class="fa fa-lg fa-search"></i></button>
+                        <div class="search-backdrop"></div>
+                        <div class="search-result">
+                            <div class="search-header">
+                                Histories
+                            </div>
+                            <div class="search-item">
+                                <a href="#">Kodinger.com</a>
+                                <a href="#" class="search-close"><i class="fas fa-times"></i></a>
+                            </div>
+                            <div class="search-header">
+                                Result
+                            </div>
+                            <div class="search-item">
+                                <a href="#">
+                                    <img class="mr-3 rounded" width="30" src="assets/img/products/product-3-50.png" alt="product">
+                                    oPhone S9 Limited Edition
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+                <div class="navbar-nav navbar-right">
+                    <h6 class="d-sm-none d-lg-inline-block">Hi, Ujang Maman</h6>
+                </div>
+            </nav>
+            <div class="main-sidebar sidebar-style-2 color-style-sidebar">
+                <aside id="sidebar-wrapper">
+                    <div class="sidebar-brand">
+                        <a href="index.html" style="font-size: 2em; color: white; text-transform: none"><span class="text-warning">On</span>Music</a>
+                    </div>
+                    <div class="sidebar-brand sidebar-brand-sm">
+                        <a href="index.html">St</a>
+                    </div>
+                    <ul class="sidebar-menu mx-3">
+                        <li><a class="nav-link" href="blank.html"><i class="fas fa-home text-white"></i> <span class="text-white">Dashboard</span></a></li>
+                        <li><a class="nav-link" href="credits.html"><i class="fas fa-headphones text-white"></i> <span class="text-white">Data Property Music</span></a></li>
+                        <li><a class="nav-link" href="credits.html"><i class="fas fa-users text-white"></i> <span class="text-white">Data Customer</span></a></li>
+                        <li><a class="nav-link" href="credits.html"><i class="fas fa-money-bill text-white"></i> <span class="text-white">Transaction</span></a></li>
+                        <li><a class="nav-link" href="credits.html"><i class="fas fa-file text-white"></i> <span class="text-white">Report</span></a></li>
+                        <li><a class="nav-link" href="credits.html"><i class="fas fa-sign-out-alt text-white"></i> <span class="text-white">Log-Out</span></a></li>
+                    </ul>
+                </aside>
+            </div>
