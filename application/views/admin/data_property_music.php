ac199f51833b496de597e5587edfaa5fa6278d57
Anggun Lenteraningati <anggunlentera@gmail.com>
2022-12-02 10:57:55 +0700

    add page data property music

---
+++ application/views/admin/data_property_music.php
@@ -0,0 +1,44 @@
+<div class="main-content">
+    <section class="section px-3">
+        <div class="section-header shadow" style="border-radius: 1.2em;">
+            <h1>Data Property Music</h1>
+        </div>
+        <div class="row">
+            <div class="col-12 col-md-12 col-lg-12">
+                <div class="card">
+                    <div class="buttons">
+                        <a class="btn btn-lg btn-warning text-dark font-weight-bold" style="border-radius: 1.2em;">Add Property</a>
+                    </div>
+                    <div class="card-body p-0">
+                        <div class="table-responsive">
+                            <table class="table table-bordered table-md text-center">
+                                <tr>
+                                    <th>No</th>
+                                    <th>Image</th>
+                                    <th>Name</th>
+                                    <th>Status</th>
+                                    <th>Action</th>
+                                </tr>
+                                <tr>
+                                    <td>1</td>
+                                    <td>
+                                        <img src="<?php echo base_url() ?>assets/image/img-logo-onmusic.png" width="50" />
+                                    </td>
+                                    <td>Lorem Ipsum</td>
+                                    <td>
+                                        <div class="badge badge-success">Active</div>
+                                    </td>
+                                    <td>
+                                        <a class="btn btn-success"><i class="fa fa-lg fa-eye text-white"></i></a>
+                                        <a class="btn btn-danger"><i class="fa fa-lg fa-trash-alt text-white"></i></a>
+                                        <a class="btn btn-primary"><i class="fa fa-lg fa-file-alt text-white"></i></a>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
