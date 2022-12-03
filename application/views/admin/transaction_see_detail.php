be7d7720ab6e511be752ef086de866db83c7eece
Anggun Lenteraningati <anggunlentera@gmail.com>
2022-12-03 15:08:40 +0700

    add page transaction see detail

---
+++ application/views/admin/transaction_see_detail.php
@@ -0,0 +1,55 @@
+<div class="main-content">
+    <section class="section px-3">
+        <div class="section-header shadow" style="border-radius: 1.2em;">
+            <h1>Detail Transaction Property</h1>
+        </div>
+        <div class="row">
+            <div class="col-12 col-md-12 col-lg-12">
+                <div class="card">
+                    <div class="card-body p-0">
+                        <div class="table-responsive">
+                            <table class="table table-bordered table-md text-center">
+                                <tr>
+                                    <th>No</th>
+                                    <th>Image</th>
+                                    <th>Name</th>
+                                    <th>Price Rent</th>
+                                    <th>Action</th>
+                                </tr>
+                                <tr>
+                                    <td>1</td>
+                                    <td>
+                                        <img src="<?php echo base_url() ?>assets/image/img-logo-onmusic.png" width="50" />
+                                    </td>
+                                    <td>Name</td>
+                                    <td>
+                                        Price Rent
+                                    </td>
+                                    <td>
+                                        <a class="btn btn-round btn-success text-white">See Detail</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>
+                                        <img src="<?php echo base_url() ?>assets/image/img-logo-onmusic.png" width="50" />
+                                    </td>
+                                    <td>Name</td>
+                                    <td>
+                                        Price Rent
+                                    </td>
+                                    <td>
+                                        <a class="btn btn-round btn-success text-white">See Detail</a>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="text-right font-weight-bold p-5">
+                        Total Payment : xx.xxx.xxx
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
