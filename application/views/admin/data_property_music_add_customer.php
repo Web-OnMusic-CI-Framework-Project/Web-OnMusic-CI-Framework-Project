697282fb34e44d6886b73957d565b03bf40672ef
Anggun Lenteraningati <anggunlentera@gmail.com>
2022-12-03 14:47:15 +0700

    add page data property music add customer

---
+++ application/views/admin/data_property_music_add_customer.php
@@ -0,0 +1,39 @@
+<div class="main-content">
+  <section class="section px-3">
+    <div class="section-header shadow" style="border-radius: 1.2em;">
+      <h1>Form Input Property Customer</h1>
+    </div>
+    <div class="row">
+      <div class="col-12 col-md-12 col-lg-12">
+        <div class="card">
+          <div class="card-body">
+            <div class="form-group">
+              <label class="font-weight-bold">Name</label>
+              <input type="text" class="form-control">
+            </div>
+            <div class="form-group">
+              <label class="font-weight-bold">Username</label>
+              <input type="text" class="form-control">
+            </div>
+            <div class="form-group">
+              <label class="font-weight-bold">Password</label>
+              <input type="password" class="form-control" data-indicator="pwindicator">
+            </div>
+            <div class="form-group">
+              <label class="font-weight-bold">Phone</label>
+              <input type="text" class="form-control phone-number" data-indicator="pwindicator">
+            </div>
+            <div class="form-group">
+              <label class="font-weight-bold">Address</label>
+              <textarea class="form-control"></textarea>
+            </div>
+            <div class="form-group mt-5">
+              <button type="submit" class="btn btn-lg btn-round btn-warning">Save</button>
+              <button type="submit" class="btn btn-lg btn-round border-warning">Reset</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
