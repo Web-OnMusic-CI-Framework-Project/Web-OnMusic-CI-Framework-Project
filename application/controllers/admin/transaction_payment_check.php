efc48f786450e4480bffc4f61bdc75dcb7d1ef64
Anggun Lenteraningati <anggunlentera@gmail.com>
2022-12-03 16:02:23 +0700

    add controller transaction payment check

---
+++ application/controllers/admin/transaction_payment_check.php
@@ -0,0 +1,11 @@
+<?php
+class Transaction_Payment_Check extends CI_Controller
+{
+    public function index()
+    {
+        $this->load->view('templates_admin/header');
+        $this->load->view('templates_admin/sidebar');
+        $this->load->view('admin/transaction_payment_check');
+        $this->load->view('templates_admin/footer');
+    }
+}
