bf8e55850b1a5d855038efe0ff91fa5913870af1
Anggun Lenteraningati <anggunlentera@gmail.com>
2022-12-04 09:48:39 +0700

    add folder customer in controller and view, create dashboard custumer

---
+++ application/views/customer/dashboard.php
@@ -0,0 +1,61 @@
+        <!-- Header-->
+        <header class="bg-dark py-5">
+            <div class="container px-4 px-lg-5 my-5">
+                <div class="text-center text-white">
+                    <h1 class="display-4 fw-bolder">Shop in style</h1>
+                    <p class="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
+                </div>
+            </div>
+        </header>
+        <!-- Section-->
+        <section class="py-5">
+            <div class="container px-4 px-lg-5 mt-5">
+                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-4 row-cols-xl-4 justify-content-center">
+                    <div class="col-md-4 mb-5">
+                        <div class="card h-100 bg-color-product" style="border-radius: 1.5em;">
+                            <!-- Product image-->
+                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." style="border-radius: 1.5em;" />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center text-white">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder">Fancy Product</h5>
+                                    <!-- Product price-->
+                                    $120.00 - $280.00
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-5">
+                        <div class="card h-100 bg-color-product" style="border-radius: 1.5em;">
+                            <!-- Product image-->
+                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." style="border-radius: 1.5em;" />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center text-white">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder">Fancy Product</h5>
+                                    <!-- Product price-->
+                                    $120.00 - $280.00
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-5">
+                        <div class="card h-100 bg-color-product" style="border-radius: 1.5em;">
+                            <!-- Product image-->
+                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." style="border-radius: 1.5em;" />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center text-white">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder">Fancy Product</h5>
+                                    <!-- Product price-->
+                                    $120.00 - $280.00
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
---
+++ application/views/templates_customer/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <title>General Dashboard &mdash; Stisla</title>
+
+    <!-- General CSS Files -->
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets/assets_stisla/modules/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets/assets_stisla/modules/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets/assets_stisla/modules/ionicons/css/ionicons.min.css">
+
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets/assets_stisla/css/style.css">
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets/assets_stisla/css/components.css">
+
+    <style>
+        .bg-color-navbar {
+            background-image: linear-gradient(to right, #FF0642, #FB0D68, #C60C7D, #BB137E);
+        }
+
+        .bg-color-footer {
+            background-image: linear-gradient(#FF0642, #FB0D68, #C60C7D, #BB137E);
+        }
+
+        .bg-color-product {
+            background-image: linear-gradient(to right, #FF0642, #FB0D68, #C60C7D, #BB137E);
+        }
+    </style>
+    <!-- Start GA -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-94034622-3');
+    </script>
+    <!-- /END GA -->
+</head>
