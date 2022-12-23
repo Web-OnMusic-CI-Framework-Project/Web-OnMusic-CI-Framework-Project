10f420401747a7db5e0bcf8f47b510ab9ecf6fa3
Anggun Lenteraningati <anggunlentera@gmail.com>
2022-12-24 02:04:50 +0700

    modified controller data property music

--- application/controllers/admin/data_property_music.php
+++ application/controllers/admin/data_property_music.php
@@ -3,9 +3,10 @@ class Data_Property_Music extends CI_Controller
 {
     public function index()
     {
+        $data['alatmusikjasa'] = $this->model_alat_musik->get_data('alatmusikjasa')->result();
         $this->load->view('templates_admin/header');
         $this->load->view('templates_admin/sidebar');
-        $this->load->view('admin/data_property_music');
+        $this->load->view('admin/data_property_music', $data);
         $this->load->view('templates_admin/footer');
     }
 }
