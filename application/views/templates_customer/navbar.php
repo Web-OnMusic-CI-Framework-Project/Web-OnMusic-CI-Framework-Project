f0a59189c115cd2c3ed36aabd88736dcbbb6d134
Anggun Lenteraningati <anggunlentera@gmail.com>
2022-12-26 11:04:09 +0700

    modified dashboard customer

--- application/views/templates_customer/navbar.php
+++ application/views/templates_customer/navbar.php
@@ -1,20 +1,12 @@
 <body class="layout-3">
-    <div id="app">
-        <div class="main-wrapper container">
-            <nav class="navbar navbar-expand-lg main-navbar bg-color-navbar">
-                <img src="<?php echo base_url() ?>assets/image/img-logo-onmusic.png" class="sidebar-gone-hide mr-2" width="40px" />
-                <a href="index.html" style="color: white; text-transform: none;" class="navbar-brand"><span class="text-warning">On</span>Music</a>
-                <a href="#" class="nav-link sidebar-gone-show" data-toggle="sidebar"><i class="fas fa-bars"></i></a>
-                <!-- <div class="nav-collapse">
-                    <a class="sidebar-gone-show nav-collapse-toggle nav-link" href="#">
-                        <i class="fas fa-ellipsis-v"></i>
-                    </a>
-                    <ul class="navbar-nav">
-                        <li class="nav-item active"><a href="#" class="nav-link">Application</a></li>
-                        <li class="nav-item"><a href="#" class="nav-link">Report Something</a></li>
-                        <li class="nav-item"><a href="#" class="nav-link">Server Status</a></li>
-                    </ul>
-                </div> -->
+    <header>
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-color-navbar">
+            <img src="<?php echo base_url() ?>assets/image/img-logo-onmusic.png" class="sidebar-gone-hide mr-2" width="40px" />
+            <a href="index.html" style="color: white; text-transform: none;" class="navbar-brand"><span class="text-warning">On</span>Music</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
                 <ul class="navbar-nav navbar-right ml-auto">
                     <li class="nav-item active"><a href="#" class="nav-link">Home</a></li>
                     <li class="nav-item"><a href="#" class="nav-link">Checkout</a></li>
@@ -22,6 +14,6 @@
                     <li class="nav-item"><a href="#" class="nav-link">Register</a></li>
                     <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
                 </ul>
-            </nav>
-        </div>
-    </div>
+            </div>
+        </nav>
+    </header>
