66079598a46764d6ad24bc6ab4e135c8282d80f2
Katon Wahyudi <katonwahyudi@gmail.com>
2022-12-28 03:25:22 +0700

    feat: add create user from admin

--- application/controllers/admin/data_customer.php
+++ application/controllers/admin/data_customer.php
@@ -10,11 +10,60 @@ class Data_Customer extends CI_Controller
         $this->load->view('templates_admin/footer');
     }
 
-    public function data_property_music_add_customer()
+    public function create_customer()
     {
         $this->load->view('templates_admin/header');
         $this->load->view('templates_admin/sidebar');
         $this->load->view('admin/data_property_music_add_customer');
         $this->load->view('templates_admin/footer');
+    }
+
+    public function create_customer_action()
+    {
+        $this->load->helper('form');
+        $this->load->library('form_validation');
+
+        $this->_rules();
+
+        if ($this->form_validation->run() === FALSE)
+        {
+            $this->create_customer();
+        }
+        else
+        {
+            $Nama               = $this->input->post('Nama');
+            $Username              = $this->input->post('Username');
+            $Password          = $this->input->post('Password');
+            $Email             = $this->input->post('Email');
+            $NoTelp             = $this->input->post('NoTelp');
+            $Alamat             = $this->input->post('Alamat');
+
+            $data = array(
+                'Nama'                  => $Nama,
+                'Username'                 => $Username,
+                'Password'             => $Password,
+                'Email'                => $Email,
+                'NoTelp'                => $NoTelp,
+                'Alamat'                => $Alamat,
+            );
+
+            $this->data_user->insert_data($data, 'user');
+            $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-dismissible fade show" role="alert">
+            Data User Berhasil Ditambahkan!.
+            <button type="buton" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </div>');
+            redirect('admin/data_customer');
+        }
+    }
+
+    public function _rules()
+    {
+        $this->form_validation->set_rules('Nama', 'Nama', 'required');
+        $this->form_validation->set_rules('Username', 'Username', 'required');
+        $this->form_validation->set_rules('Password', 'Password', 'required');
+        $this->form_validation->set_rules('Email', 'Email', 'required');
+        $this->form_validation->set_rules('NoTelp', 'NoTelp', 'required');
+        $this->form_validation->set_rules('Alamat', 'Alamat', 'required');
     }
 }
--- application/views/admin/data_customer.php
+++ application/views/admin/data_customer.php
@@ -8,12 +8,14 @@
                 <div class="card">
                     <div class="card-body p-0">
                         <div class="table-responsive p-3">
-                            <a class="btn btn-lg btn-warning text-dark font-weight-bold mb-3" href="<?php echo base_url('admin/data_customer/data_property_music_add_customer') ?>" style="border-radius: 1.2em;">Add Customer</a>
+                            <a class="btn btn-lg btn-warning text-dark font-weight-bold mb-3" href="<?php echo base_url('admin/data_customer/create_customer') ?>" style="border-radius: 1.2em;">Add Customer</a>
+                            <?php echo $this->session->flashdata('pesan') ?>
                             <table class="table table-bordered table-md text-center">
                                 <thead>
                                     <th width="20">No</th>
                                     <th>Name</th>
                                     <th>Username</th>
+                                    <th>Email</th>
                                     <th>Address</th>
                                     <th>Phone</th>
                                     <th>Action</th>
@@ -27,11 +29,12 @@
                                             <td><?php echo $no++ ?></td>
                                             <td><?php echo $us->Nama ?></td>
                                             <td><?php echo $us->Username ?></td>
+                                            <td><?php echo $us->Email ?></td>
                                             <td><?php echo $us->Alamat ?></td>
                                             <td><?php echo $us->NoTelp ?></td>
                                             <td>
-                                                <a class="btn btn-primary"><i class="fa fa-lg fa-file-alt text-white"></i></a>
-                                                <a class="btn btn-danger"><i class="fa fa-lg fa-trash-alt text-white"></i></a>
+                                                <a href="<?php echo base_url('admin/data_customer/update_data') . $us->id_user ?>" class="btn btn-primary"><i class="fa fa-lg fa-pen-alt text-white"></i></a>
+                                                <a href="<?php echo base_url('admin/data_customer/delete_data') . $us->id_user ?>" class="btn btn-danger"><i class="fa fa-lg fa-trash-alt text-white"></i></a>
                                             </td>
                                         </tr>
                                     <?php endforeach; ?>
