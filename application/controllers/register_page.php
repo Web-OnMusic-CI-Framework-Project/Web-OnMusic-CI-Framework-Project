42bc87a2f1acc1af12ead31452ca861c917db141
Katon Wahyudi <katonwahyudi@gmail.com>
2022-12-28 21:13:04 +0700

    style : modified register page

--- application/controllers/register_page.php
+++ application/controllers/register_page.php
@@ -3,6 +3,8 @@ class Register_Page extends CI_Controller
 {
     public function index()
     {
+        $this->load->view('templates_customer/header');
         $this->load->view('register_page');
+        $this->load->view('templates_customer/footer');
     }
 }
